59ac9c60bdc164952fd261f1d8b2fcfdade91b46
VladanKozic <mr20024@alas.matf.bg.ac.rs>
2024-04-11 03:39:29 +0200

    Dodati modeli watchtower, ammo box, crates and barrels i drvece (mali problem sa teksturom grancica)

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # project_base
 Prazan projekat sa pratećim bibliotekama koje koristimo na času. 
 
-"Tank T-10M" (https://skfb.ly/6QUSX) by yanix is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+"Tank T-10M" (https://skfb.ly/6QUSX) by yanix is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).\
+https://www.independent-software.com/tileable-repeatable-hires-terrain-textures-for-download.html\
+"Crates And Barrels" (https://skfb.ly/oKL8Q) by Mateusz Woliński is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).\
+"Watchtower" (https://skfb.ly/oSsKN) by adventurer is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).\
+"Ammo Box" (https://skfb.ly/6ZBFQ) by murilojones is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).\
+"Redwood Tree 2" (https://skfb.ly/6WVrV) by rhcreations is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).\
+"Pine tree low-poly" (https://skfb.ly/6Z6wM) by Ricardo Sanchez is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).\
+"Pine Tree" (https://skfb.ly/ouSXM) by evolveduk is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).\
--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,13 @@
 #include <iostream>
 #include "learnopengl/filesystem.h"
 
+#define GROUND_DIMENSION (10)
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
+unsigned int loadTexture(char const *path);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -76,14 +79,73 @@ int main()
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
+    // ground texture
+
+    float groundVertices[] = {
+            // positions          // normals        //texCoords
+            0.5f,  0.5f,  0.0f,   0.0f, 0.0f, -1.0f, 1.0f,  1.0f,     // top right
+            0.5f, -0.5f,  0.0f,   0.0f, 0.0f, -1.0f, 1.0f,  0.0f,     // bottom right
+            -0.5f, -0.5f,  0.0f,  0.0f, 0.0f, -1.0f, 0.0f,  0.0f,     // bottom left
+            -0.5f,  0.5f,  0.0f,  0.0f, 0.0f, -1.0f, 0.0f,  1.0f      // top left
+    };
+
+    unsigned int groundIndices[] = {
+            0, 1, 3,  // first Triangle
+            1, 2, 3   // second Triangle
+    };
+
+    unsigned int groundVAO, groundVBO, groundEBO;
+    glGenVertexArrays(1, &groundVAO);
+    glGenBuffers(1, &groundVBO);
+    glGenBuffers(1, &groundEBO);
+
+    glBindVertexArray(groundVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, groundVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(groundVertices), groundVertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, groundEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(groundIndices), groundIndices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    unsigned int diffuseGround = loadTexture(FileSystem::getPath("resources/textures/tough_grass.jpg").c_str());
+    //unsigned int specularGround = loadTexture(FileSystem::getPath("resources/textures/Pebbles_Height.png").c_str());
+
     // build and compile shaders
     // -------------------------
-    Shader ourShader("/home/vkozic/Desktop/RGProjekat/resources/shaders/lightningShader.vs", "/home/vkozic/Desktop/RGProjekat/resources/shaders/lightningShader.fs");
+    Shader lightningShader("/home/vkozic/Desktop/RGProjekat/resources/shaders/lightningShader.vs", "/home/vkozic/Desktop/RGProjekat/resources/shaders/lightningShader.fs");
 
     // load models
     // -----------
-    Model ourModel(FileSystem::getPath("resources/objects/sun/sun.obj"));
-    ourModel.SetShaderTextureNamePrefix("material.");
+    Model tankModel(FileSystem::getPath("resources/objects/tank_t10m/tank_t10m.obj"));
+    tankModel.SetShaderTextureNamePrefix("material.");
+
+    Model ammoBoxModel(FileSystem::getPath("resources/objects/ammo_box/ammo_box.obj"));
+    ammoBoxModel.SetShaderTextureNamePrefix("material.");
+
+    Model watchtowerModel(FileSystem::getPath("resources/objects/watchtower/watchtower.obj"));
+    watchtowerModel.SetShaderTextureNamePrefix("material.");
+
+    Model cratesAndBarrelsModel(FileSystem::getPath("resources/objects/crates_and_barrels/crates_and_barrels.obj"));
+    cratesAndBarrelsModel.SetShaderTextureNamePrefix("material.");
+
+    Model redwoodTreeModel(FileSystem::getPath("resources/objects/redwood_tree/redwood_tree.obj"));
+    redwoodTreeModel.SetShaderTextureNamePrefix("material.");
+
+    Model pineTreeModel(FileSystem::getPath("resources/objects/pine_tree/pine_tree.obj"));
+    pineTreeModel.SetShaderTextureNamePrefix("material.");
+
+    Model pineTreeLowpolyModel(FileSystem::getPath("resources/objects/pine_tree_low-poly/pine_tree_low-poly.obj"));
+    pineTreeLowpolyModel.SetShaderTextureNamePrefix("material.");
 
     // draw in wireframe
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -108,28 +170,104 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // don't forget to enable shader before setting uniforms
-        ourShader.use();
+        lightningShader.use();
         // dodavanje svetla
-        ourShader.setVec3("viewPosition", camera.Position);
-        ourShader.setFloat("material.shininess", 32.0f);
+        lightningShader.setVec3("viewPosition", camera.Position);
+        lightningShader.setFloat("material.shininess", 32.0f);
 
-        ourShader.setVec3("dirLight.direction", -1.0f, 0.0f, 0.0f);
-        ourShader.setVec3("dirLight.ambient", 1.0f, 1.0f, 1.0f);
-        ourShader.setVec3("dirLight.diffuse", 1.0f, 1.0f, 1.0f);
-        ourShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+        lightningShader.setVec3("dirLight.direction", -1.0f, 0.0f, 0.0f);
+        lightningShader.setVec3("dirLight.ambient", 1.0f, 1.0f, 1.0f);
+        lightningShader.setVec3("dirLight.diffuse", 1.0f, 1.0f, 1.0f);
+        lightningShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
-        ourShader.setMat4("projection", projection);
-        ourShader.setMat4("view", view);
+        lightningShader.setMat4("projection", projection);
+        lightningShader.setMat4("view", view);
 
-        // render the loaded model
+        // render tank
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));	// it's a bit too big for our scene, so scale it down
-        ourShader.setMat4("model", model);
-        ourModel.Draw(ourShader);
+        model = glm::translate(model, glm::vec3(0.0f, -2.0f, -10.0f)); // translate it down so it's at the center of the scene
+        model = glm::rotate(model, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        //model = glm::scale(model, glm::vec3(1.0f, 0.3f, 0.3f));	// it's a bit too big for our scene, so scale it down
+        lightningShader.setMat4("model", model);
+        tankModel.Draw(lightningShader);
+
+        // render ammo box
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-7.0f, -2.0f, -12.0f)); // translate it down so it's at the center of the scene
+        //model = glm::rotate(model, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.02f, 0.02f, 0.02f));	// it's a bit too big for our scene, so scale it down
+        lightningShader.setMat4("model", model);
+        ammoBoxModel.Draw(lightningShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-6.32f, -2.0f, -11.83f)); // translate it down so it's at the center of the scene
+        model = glm::rotate(model, glm::radians(-30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.02f, 0.02f, 0.02f));	// it's a bit too big for our scene, so scale it down
+        lightningShader.setMat4("model", model);
+        ammoBoxModel.Draw(lightningShader);
+
+        // render watchtower
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-9.0f, -2.0f, -17.0f)); // translate it down so it's at the center of the scene
+        model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.03f, 0.03f, 0.03f));	// it's a bit too big for our scene, so scale it down
+        lightningShader.setMat4("model", model);
+        watchtowerModel.Draw(lightningShader);
+
+        // render crates and barrels
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(7.0f, -2.0f, -17.0f)); // translate it down so it's at the center of the scene
+        model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));	// it's a bit too big for our scene, so scale it down
+        lightningShader.setMat4("model", model);
+        cratesAndBarrelsModel.Draw(lightningShader);
+
+        // render trees
+        /*
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));	// it's a bit too big for our scene, so scale it down
+        lightningShader.setMat4("model", model);
+        redwoodTreeModel.Draw(lightningShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-7.0f, -2.0f, 0.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));	// it's a bit too big for our scene, so scale it down
+        lightningShader.setMat4("model", model);
+        pineTreeModel.Draw(lightningShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(7.0f, -2.0f, 0.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));	// it's a bit too big for our scene, so scale it down
+        lightningShader.setMat4("model", model);
+        pineTreeLowpolyModel.Draw(lightningShader);
+*/
+
+        // render ground texture
+        lightningShader.use();
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, diffuseGround);
+
+        for(int i = 0; i < GROUND_DIMENSION; i ++) {
+            for(int j = 0; j < GROUND_DIMENSION; j ++) {
+                model = glm::mat4(1.0f);
+                model = glm::translate(model, glm::vec3(((float)i - GROUND_DIMENSION / 2.0f) * 2.0f, -2.0f, j * (-2.0f)));
+                model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+                model = glm::scale(model, glm::vec3(2.0f));
+                lightningShader.setMat4("model", model);
+                lightningShader.setMat4("view", view);
+                lightningShader.setMat4("projection", projection);
+
+                glBindVertexArray(groundVAO);
+                glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+            }
+        }
+
+
 
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -198,4 +336,42 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+unsigned int loadTexture(char const *path){
+    unsigned textureId;
+    glGenTextures(1, &textureId);
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
+
+    if(data)
+    {
+        GLenum format = GL_RED;
+        if(nrChannels == 1)
+            format = GL_RED;
+        else if(nrChannels == 3)
+            format = GL_RGB;
+        else if(nrChannels == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureId);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else{
+        cout << "Texture failed to load!" << path << "\n";
+        stbi_image_free(data);
+    }
+
+    return textureId;
 }
